7b8df69b910a5ff56140ffd8bf1dbfe4b146428e
wunmao <wunmao@live.com>
2018-07-11 10:33:18 +0800

    20180711

--- GPGO_MultiPLCs/App.xaml.cs
+++ GPGO_MultiPLCs/App.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.ServiceProcess;
-using System.Windows;
-using GPGO_MultiPLCs.Helpers;
+﻿using GPGO_MultiPLCs.Helpers;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace GPGO_MultiPLCs
 {
@@ -14,6 +17,40 @@ namespace GPGO_MultiPLCs
     {
         public App()
         {
+            var temp = Process.GetProcesses();
+            var mp = Process.GetCurrentProcess();
+
+            temp.Where(p => p.ProcessName.ToLower().Contains("gpgo_multiplcs") && p.Id != mp.Id).ToList().ForEach(p => p.Kill());
+
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                ErrorRecoder.RecordError((Exception)e.ExceptionObject);
+
+                if (e.IsTerminating)
+                {
+                    //Process.Start(ResourceAssembly.Location);
+                    //Current.Shutdown();
+                }
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                ErrorRecoder.RecordError(e.Exception);
+                e.SetObserved();
+
+                //Process.Start(ResourceAssembly.Location);
+                //Current.Shutdown();
+            };
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                e.Handled = true;
+                ErrorRecoder.RecordError(e.Exception);
+
+                //Process.Start(ResourceAssembly.Location);
+                //Current.Shutdown();
+            };
+
             //!以下方式在win10時，須保證在系統管理員權限下執行才可成功，可在app.config中加入<requestedExecutionLevel level="requireAdministrator" uiAccess="false" />，
             //!但一旦指定為系統管理員權限，就無法再start menu中自動啟動，須改用工作管理員設定為登入後啟動
 
