e786ebc26e43017698ccac44fc8ef7e2ed573d87
wunmao <wunmao@live.com>
2018-07-29 22:27:18 +0800

    20180729

--- GPGO_MultiPLCs/ViewModels/TraceabilityView_ViewModel.cs
+++ GPGO_MultiPLCs/ViewModels/TraceabilityView_ViewModel.cs
@@ -14,6 +14,7 @@ using OfficeOpenXml.Style;
 using OxyPlot;
 using OxyPlot.Axes;
 using OxyPlot.Series;
+//using Newtonsoft.Json;
 
 namespace GPGO_MultiPLCs.ViewModels
 {
@@ -827,12 +828,8 @@ namespace GPGO_MultiPLCs.ViewModels
 
             ToExcelCommand = new RelayCommand(o =>
                                               {
-                                                  //using (var sw = File.AppendText("ooxx.json"))
-                                                  //{
-                                                  //    sw.WriteLine(JsonConvert.SerializeObject(ViewResults, Formatting.Indented));
-                                                  //    sw.Flush();
-                                                  //    sw.Close();
-                                                  //}
+                                                  //File.WriteAllText("ooxx.json", JsonConvert.SerializeObject(ViewResults, Formatting.Indented));
+
                                                   SaveToExcel(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
                                               });
 
