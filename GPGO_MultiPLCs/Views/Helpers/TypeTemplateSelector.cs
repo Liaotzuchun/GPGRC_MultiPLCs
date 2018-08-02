7c34809012a8690b3772c0f34f641f5d284852af
wunmao <wunmao@live.com>
2018-08-02 12:06:10 +0800

    20180802

---
+++ GPGO_MultiPLCs/Views/Helpers/TypeTemplateSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GPGO_MultiPLCs.Views
+{
+    public class CheckTypeTemplateSelector : DataTemplateSelector
+    {
+        public Type DataType { get; set; }
+        public DataTemplate Template { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item != null && (container is FrameworkElement && item.GetType() == DataType))
+            {
+                return Template;
+            }
+
+            return null;
+        }
+    }
+}
