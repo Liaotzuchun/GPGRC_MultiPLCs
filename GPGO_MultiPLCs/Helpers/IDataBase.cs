3104cf2ebd62a9c6c70c7d244b55557f0c1b8a33
wunmao <wunmao@live.com>
2018-08-13 15:17:51 +0800

    20180813

--- GPGO_MultiPLCs/Helpers/IDataBase.cs
+++ GPGO_MultiPLCs/Helpers/IDataBase.cs
@@ -6,38 +6,94 @@ using MongoDB.Driver;
 
 namespace GPGO_MultiPLCs.Helpers
 {
+    /// <summary>資料庫基本介面</summary>
+    /// <typeparam name="T"></typeparam>
     public interface IDataBase<T> where T : class, new()
     {
+        /// <summary>新增資料</summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         bool Add(T data);
 
+        /// <summary>(非同步)新增資料</summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         Task<bool> AddAsync(T data);
 
+        /// <summary>新增數筆資料</summary>
+        /// <param name="datas"></param>
+        /// <returns></returns>
         bool AddMany(ICollection<T> datas);
 
+        /// <summary>(非同步)新增數筆資料</summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         Task<bool> AddManyAsync(ICollection<T> data);
 
+        /// <summary>刪除符合條件之資料</summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
         bool Delete(Expression<Func<T, bool>> condition);
 
+        /// <summary>(非同步)刪除符合條件之資料</summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
         Task<bool> DeleteAsync(Expression<Func<T, bool>> condition);
 
+        /// <summary>刪除符合條件之單一資料</summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
         bool DeleteOne(Expression<Func<T, bool>> condition);
 
+        /// <summary>(非同步)刪除符合條件之單一資料</summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
         Task<bool> DeleteOneAsync(Expression<Func<T, bool>> condition);
 
+        /// <summary>尋找符合條件之所有資料</summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
         List<T> Find(Expression<Func<T, bool>> condition);
 
+        /// <summary>(非同步)尋找符合條件之所有資料</summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
         Task<List<T>> FindAsync(Expression<Func<T, bool>> condition);
 
+        /// <summary>尋找符合條件之單一資料</summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
         T FindOne(Expression<Func<T, bool>> condition);
 
+        /// <summary>(非同步)尋找符合條件之單一資料</summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
         Task<T> FindOneAsync(Expression<Func<T, bool>> condition);
 
+        /// <summary>更新符合條件之單一資料的特定屬性</summary>
+        /// <param name="condition"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         bool UpdateOne(Expression<Func<T, bool>> condition, string propertyName, object value);
 
+        /// <summary>(非同步)更新符合條件之單一資料的特定屬性</summary>
+        /// <param name="condition"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         Task<bool> UpdateOneAsync(Expression<Func<T, bool>> condition, string propertyName, object value);
 
+        /// <summary>更新單一資料，若資料庫中本來沒有則會自動新增</summary>
+        /// <param name="condition"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
         bool Upsert(Expression<Func<T, bool>> condition, T data);
 
+        /// <summary>(非同步)更新單一資料，若資料庫中本來沒有則會自動新增</summary>
+        /// <param name="condition"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
         Task<bool> UpsertAsync(Expression<Func<T, bool>> condition, T data);
     }
 
@@ -133,6 +189,8 @@ namespace GPGO_MultiPLCs.Helpers
 
         private readonly IMongoCollection<T> MongoCollection;
 
+        /// <summary>創建MongoDB資料庫集合檢視</summary>
+        /// <param name="collection">MongoDB資料集合</param>
         public MongoBase(IMongoCollection<T> collection)
         {
             MongoCollection = collection;
--- GPGO_MultiPLCs/Helpers/ObservableObject.cs
+++ GPGO_MultiPLCs/Helpers/ObservableObject.cs
@@ -10,6 +10,10 @@ namespace GPGO_MultiPLCs.Helpers
         public event PropertyChangedEventHandler PropertyChanged;
         private readonly Dictionary<string, object> _properties = new Dictionary<string, object>();
 
+        /// <summary>取得欄位值</summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name">欄位名稱</param>
+        /// <returns></returns>
         protected T Get<T>([CallerMemberName] string name = "")
         {
             if (_properties.TryGetValue(name, out var value))
@@ -25,17 +29,29 @@ namespace GPGO_MultiPLCs.Helpers
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        /// <summary>設定欄位值</summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">設定值</param>
+        /// <param name="name">欄位名稱</param>
         protected void Set<T>(T value, [CallerMemberName] string name = "")
         {
             _properties[name] = value;
             NotifyPropertyChanged(name);
         }
 
+        /// <summary>設定欄位值，但不產生值變通知</summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">設定值</param>
+        /// <param name="name">欄位名稱</param>
         protected void Set_WithOutNotify<T>(T value, [CallerMemberName] string name = "")
         {
             _properties[name] = value;
         }
 
+        /// <summary>設定欄位值，當值相同時不更改值也不通知</summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">設定值</param>
+        /// <param name="name">欄位名稱</param>
         protected void Set_WithOutNotifyWhenEquals<T>(T value, [CallerMemberName] string name = "")
         {
             if (Equals(value, Get<T>(name)))
