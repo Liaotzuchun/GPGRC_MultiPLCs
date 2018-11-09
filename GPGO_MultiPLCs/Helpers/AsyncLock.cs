5de74af092f6795153d34ab413e6a7d0d45d1273
wunmao <wunmao@live.com>
2018-11-09 09:03:57 +0800

    20181109 加入一些非同步擴充方法和實用類別

---
+++ GPGO_MultiPLCs/Helpers/AsyncLock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GPGO_MultiPLCs.Helpers
+{
+    public class AsyncLock
+    {
+        private readonly Task<Releaser> m_releaser;
+
+        private readonly AsyncSemaphore m_semaphore;
+
+        public Task<Releaser> LockAsync()
+        {
+            var wait = m_semaphore.WaitAsync();
+
+            return wait.IsCompleted ? m_releaser : wait.ContinueWith((_, state) => new Releaser((AsyncLock)state),
+                                                                     this,
+                                                                     CancellationToken.None,
+                                                                     TaskContinuationOptions.ExecuteSynchronously,
+                                                                     TaskScheduler.Default);
+        }
+
+        public struct Releaser : IDisposable
+        {
+            private readonly AsyncLock m_toRelease;
+
+            internal Releaser(AsyncLock toRelease)
+            {
+                m_toRelease = toRelease;
+            }
+
+            public void Dispose()
+            {
+                m_toRelease?.m_semaphore.Release();
+            }
+        }
+
+        public AsyncLock()
+        {
+            m_semaphore = new AsyncSemaphore(1);
+            m_releaser = Task.FromResult(new Releaser(this));
+        }
+    }
+}
---
+++ GPGO_MultiPLCs/Helpers/WaitHandleExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace GPGO_MultiPLCs.Helpers
+{
+    public static class WaitHandleExtensions
+    {
+        public static Task WaitOneAsync(this WaitHandle waitHandle, CancellationToken cancellationToken)
+        {
+            return WaitOneAsync(waitHandle, Timeout.Infinite, cancellationToken);
+        }
+
+        public static async Task<bool> WaitOneAsync(this WaitHandle waitHandle, int timeout, CancellationToken cancellationToken)
+        {
+            if (waitHandle is Mutex)
+            {
+                return false;
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var tcs = new TaskCompletionSource<bool>();
+            var rwh = ThreadPool.RegisterWaitForSingleObject(waitHandle,
+                                                             (state, timedOut) =>
+                                                             {
+                                                                 ((TaskCompletionSource<bool>)state).SetResult(!timedOut);
+                                                             },
+                                                             tcs,
+                                                             timeout,
+                                                             true);
+
+            using (cancellationToken.Register(() =>
+                                              {
+                                                  if (rwh.Unregister(null))
+                                                  {
+                                                      tcs.SetCanceled();
+                                                  }
+                                              }))
+            {
+                try
+                {
+                    return await tcs.Task.ConfigureAwait(false);
+                }
+                finally
+                {
+                    rwh.Unregister(null);
+                }
+            }
+        }
+    }
+}
