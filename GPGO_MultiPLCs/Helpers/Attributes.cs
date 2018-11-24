dfc18eb4b35baf87bdb0b092402195fcdedcc83d
wunmao <wunmao@live.com>
2018-11-24 20:12:41 +0800

    20181124

--- GPGO_MultiPLCs/Helpers/Attributes.cs
+++ GPGO_MultiPLCs/Helpers/Attributes.cs
@@ -4,12 +4,12 @@ using GPGO_MultiPLCs.Models;
 namespace GPGO_MultiPLCs.Helpers
 {
     [AttributeUsage(AttributeTargets.Class)]
-    public class OrderedObject : Attribute
+    public sealed class OrderedObject : Attribute
     {
     }
 
     [AttributeUsage(AttributeTargets.Property)]
-    public class OrderIndex : Attribute
+    public sealed class OrderIndex : Attribute
     {
         public int Index { get; }
 
@@ -20,7 +20,7 @@ namespace GPGO_MultiPLCs.Helpers
     }
 
     [AttributeUsage(AttributeTargets.Property)]
-    public class LanguageTranslator : Attribute
+    public sealed class LanguageTranslator : Attribute
     {
         public string CHS_Name { get; }
         public string CHT_Name { get; }
@@ -38,52 +38,4 @@ namespace GPGO_MultiPLCs.Helpers
             CHS_Name = chs;
         }
     }
-
-    //[AttributeUsage(AttributeTargets.Property)]
-    //public class EN_Name : Attribute
-    //{
-    //    public string Name { get; }
-
-    //    public override string ToString()
-    //    {
-    //        return Name;
-    //    }
-
-    //    public EN_Name(string name)
-    //    {
-    //        Name = name;
-    //    }
-    //}
-
-    //[AttributeUsage(AttributeTargets.Property)]
-    //public class CHT_Name : Attribute
-    //{
-    //    public string Name { get; }
-
-    //    public override string ToString()
-    //    {
-    //        return Name;
-    //    }
-
-    //    public CHT_Name(string name)
-    //    {
-    //        Name = name;
-    //    }
-    //}
-
-    //[AttributeUsage(AttributeTargets.Property)]
-    //public class CHS_Name : Attribute
-    //{
-    //    public string Name { get; }
-
-    //    public override string ToString()
-    //    {
-    //        return Name;
-    //    }
-
-    //    public CHS_Name(string name)
-    //    {
-    //        Name = name;
-    //    }
-    //}
 }
