2f8f66285cd0e09224293a9f912e1b65830157de
wunmao <wunmao@live.com>
2018-12-19 16:55:43 +0800

    20181219

--- GPGO_MultiPLCs/App.xaml.cs
+++ GPGO_MultiPLCs/App.xaml.cs
@@ -64,23 +64,38 @@ namespace GPGO_MultiPLCs
                                                 //Current.Shutdown();
                                             };
 
+            //!當啟MongoDB服務或進程不存在時動MongoDB
+            if (Process.GetProcessesByName("mongod").Length == 0)
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "C:\\Program Files\\MongoDB\\Server\\4.0\\bin\\mongod.exe",
+                        Arguments = "--dbpath=D:\\GPDB\\data --logpath=D:\\GPDB\\logs\\log.txt --bind_ip_all",
+                        WindowStyle = ProcessWindowStyle.Hidden
+                    }
+                };
+                process.Start();
+            }
+
             //!以下方式在win10時，須保證在系統管理員權限下執行才可成功，可在app.config中加入<requestedExecutionLevel level="requireAdministrator" uiAccess="false" />，
             //!但一旦指定為系統管理員權限，就無法再start menu中自動啟動，須改用工作管理員設定為登入後啟動
+            //var mongo_service = new ServiceController("MongoDB");
+            //if (mongo_service.Status == ServiceControllerStatus.Stopped)
+            //{
+            //try
+            //{
+            //    mongo_service.Start();
+            //    mongo_service.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromSeconds(9));
+            //}
+            //catch (Exception ex)
+            //{
+            //    Log.Error(ex, "Mongo嘗試啟動失敗");
+            //}
+            //}
 
             BsonSerializer.RegisterSerializer(typeof(DateTime), DateTimeSerializer.LocalInstance);
-            var mongo_service = new ServiceController("MongoDB");
-            if (mongo_service.Status == ServiceControllerStatus.Stopped)
-            {
-                try
-                {
-                    mongo_service.Start();
-                    mongo_service.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromSeconds(9));
-                }
-                catch (Exception ex)
-                {
-                    Log.Error(ex, "Mongo嘗試啟動失敗");
-                }
-            }
         }
     }
 }
