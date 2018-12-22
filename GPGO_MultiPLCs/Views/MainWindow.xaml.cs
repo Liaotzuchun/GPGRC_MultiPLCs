23e50145354404e44e6963f99f0a22ec8707d818
wunmao <wunmao@live.com>
2018-12-22 10:03:12 +0800

    20181222 新增版本號

--- GPGO_MultiPLCs/Views/MainWindow.xaml.cs
+++ GPGO_MultiPLCs/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace GPGO_MultiPLCs
@@ -6,6 +7,14 @@ namespace GPGO_MultiPLCs
     /// <summary>MainWindow.xaml 的互動邏輯</summary>
     public partial class MainWindow : Window
     {
+        private void GlobalDialog_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                Authenticator.SetCurrentValue(VisibilityProperty, Visibility.Collapsed);
+            }
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             Panel.SetZIndex(PopGrid, 100);
@@ -14,14 +23,8 @@ namespace GPGO_MultiPLCs
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private void GlobalDialog_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            if (Visibility == Visibility.Visible)
-            {
-                Authenticator.SetCurrentValue(VisibilityProperty, Visibility.Collapsed);
-            }
+            Title = "群翊工業 " + Assembly.GetExecutingAssembly().GetName().Version;
         }
     }
 }
