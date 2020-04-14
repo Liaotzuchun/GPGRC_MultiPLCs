7c3c28ce4d6c42387540d97290b8281fc902046e
wunmao <wunmao@live.com>
2020-04-14 10:08:15 +0800

    20200414

--- GPGO_MultiPLCs/Models/PLC_DataProvider.cs
+++ GPGO_MultiPLCs/Models/PLC_DataProvider.cs
@@ -335,28 +335,46 @@ namespace GPGO_MultiPLCs.Models
         public async Task StartRecoder(long cycle_ms, CancellationToken ct)
         {
             OvenInfo.StartTime = DateTime.Now;
+            OvenInfo.RecordTemperatures.Clear();
 
             await OneScheduler.StartNew(() =>
                                         {
                                             var n = TimeSpan.Zero;
+                                            var _ThermostatTemperature = ThermostatTemperature;
+                                            var _OvenTemperature_1 = OvenTemperature_1;
+                                            var _OvenTemperature_2 = OvenTemperature_2;
+                                            var _OvenTemperature_3 = OvenTemperature_3;
+                                            var _OvenTemperature_4 = OvenTemperature_4;
+                                            var _OvenTemperature_5 = OvenTemperature_5;
+                                            var _OvenTemperature_6 = OvenTemperature_6;
+                                            var _OvenTemperature_7 = OvenTemperature_7;
+                                            var _OvenTemperature_8 = OvenTemperature_8;
 
                                             while (!ct.IsCancellationRequested)
                                             {
-                                                var nt = DateTime.Now;
-                                                var et = nt - OvenInfo.StartTime;
-                                                if (et >= n)
+                                                _ThermostatTemperature = ThermostatTemperature <= 0 ? _ThermostatTemperature : ThermostatTemperature;
+                                                _OvenTemperature_1 = OvenTemperature_1 <= 0 ? _OvenTemperature_1 : OvenTemperature_1;
+                                                _OvenTemperature_2 = OvenTemperature_2 <= 0 ? _OvenTemperature_2 : OvenTemperature_2;
+                                                _OvenTemperature_3 = OvenTemperature_3 <= 0 ? _OvenTemperature_3 : OvenTemperature_3;
+                                                _OvenTemperature_4 = OvenTemperature_4 <= 0 ? _OvenTemperature_4 : OvenTemperature_4;
+                                                _OvenTemperature_5 = OvenTemperature_5 <= 0 ? _OvenTemperature_5 : OvenTemperature_5;
+                                                _OvenTemperature_6 = OvenTemperature_6 <= 0 ? _OvenTemperature_6 : OvenTemperature_6;
+                                                _OvenTemperature_7 = OvenTemperature_7 <= 0 ? _OvenTemperature_7 : OvenTemperature_7;
+                                                _OvenTemperature_8 = OvenTemperature_8 <= 0 ? _OvenTemperature_8 : OvenTemperature_8;
+
+                                                if (DateTime.Now - OvenInfo.StartTime >= n)
                                                 {
                                                     AddTemperatures(OvenInfo.StartTime,
-                                                                    nt,
-                                                                    ThermostatTemperature,
-                                                                    OvenTemperature_1,
-                                                                    OvenTemperature_2,
-                                                                    OvenTemperature_3,
-                                                                    OvenTemperature_4,
-                                                                    OvenTemperature_5,
-                                                                    OvenTemperature_6,
-                                                                    OvenTemperature_7,
-                                                                    OvenTemperature_8);
+                                                                    DateTime.Now,
+                                                                    _ThermostatTemperature,
+                                                                    _OvenTemperature_1,
+                                                                    _OvenTemperature_2,
+                                                                    _OvenTemperature_3,
+                                                                    _OvenTemperature_4,
+                                                                    _OvenTemperature_5,
+                                                                    _OvenTemperature_6,
+                                                                    _OvenTemperature_7,
+                                                                    _OvenTemperature_8);
 
                                                     if (n >= TimeSpan.FromMinutes(30))
                                                     {
