4ca8f8351e914c0df7f56ef396b3bf16a92cd6cb
wunmao <wunmao@live.com>
2020-09-03 22:55:46 +0800

    20200903

--- GPGO_MultiPLCs/Views/Helpers/Converters.cs
+++ GPGO_MultiPLCs/Views/Helpers/Converters.cs
@@ -5,5 +5,18 @@ using System.Windows.Data;
 
 namespace GPGO_MultiPLCs.Views
 {
+    public class ListTakeCount : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.First() is int count)
+            {
+                return values.Skip(1).Take(count).ToList();
+            }
 
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
 }
