3986d2c8f53a50195250678b641ca3c8619b5575
wunmao <wunmao@gmail.com>
2022-01-26 11:18:43 +0800

    20220126

--- GPGO_MultiPLCs/ViewModels/PLC_ViewModel.cs
+++ GPGO_MultiPLCs/ViewModels/PLC_ViewModel.cs
@@ -5,7 +5,6 @@ using GPMVVM.PLCService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Schedulers;
