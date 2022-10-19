a450a9e5ff3aad5709e958c03dcf6b7cbd49b13e
wunmao <wunmao@live.com>
2022-10-19 19:00:01 +0800

    20221019

--- GPGO_MultiPLCs/Models/DataoutputCSV.cs
+++ GPGO_MultiPLCs/Models/DataoutputCSV.cs
@@ -13,12 +13,12 @@ namespace GPGO_MultiPLCs.Models;
 
 public class DataoutputCSV
 {
-    private string   DataTitles;
-    private string   RecordTitles;
-    private string[] _RecipeTitles;
-    private string   RecipeTitles;
-    private string   AlarmTitles;
-    private Language Language = Language.TW;
+    private string                  DataTitles;
+    private string                  RecordTitles;
+    private Dictionary<string, int> _RecipeTitles;
+    private string                  RecipeTitles;
+    private string                  AlarmTitles;
+    private Language                Language = Language.TW;
 
     private async Task DataMethod(ProcessInfo info, string folder, string filename)
     {
@@ -248,77 +248,14 @@ public class DataoutputCSV
                                                        {
                                                            try
                                                            {
-                                                               recipe.RecipeName            = col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RecipeName))[0]];
-                                                               recipe.NitrogenMode          = bool.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.NitrogenMode))[0]]);
-                                                               recipe.OxygenContentSet      = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.OxygenContentSet))[0]]);
-                                                               recipe.InflatingTime         = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.InflatingTime))[0]]);
-                                                               recipe.DwellTemperature_1    = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTemperature_1))[0]]);
-                                                               recipe.DwellTemperature_2    = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTemperature_2))[0]]);
-                                                               recipe.DwellTemperature_3    = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTemperature_3))[0]]);
-                                                               recipe.DwellTemperature_4    = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTemperature_4))[0]]);
-                                                               recipe.DwellTemperature_5    = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTemperature_5))[0]]);
-                                                               recipe.DwellTemperature_6    = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTemperature_6))[0]]);
-                                                               recipe.DwellTemperature_7    = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTemperature_7))[0]]);
-                                                               recipe.DwellTemperature_8    = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTemperature_8))[0]]);
-                                                               recipe.DwellTime_1           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTime_1))[0]]);
-                                                               recipe.DwellTime_2           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTime_2))[0]]);
-                                                               recipe.DwellTime_3           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTime_3))[0]]);
-                                                               recipe.DwellTime_4           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTime_4))[0]]);
-                                                               recipe.DwellTime_5           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTime_5))[0]]);
-                                                               recipe.DwellTime_6           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTime_6))[0]]);
-                                                               recipe.DwellTime_7           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTime_7))[0]]);
-                                                               recipe.DwellTime_8           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTime_8))[0]]);
-                                                               recipe.DwellAlarm_1          = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellAlarm_1))[0]]);
-                                                               recipe.DwellAlarm_2          = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellAlarm_2))[0]]);
-                                                               recipe.DwellAlarm_3          = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellAlarm_3))[0]]);
-                                                               recipe.DwellAlarm_4          = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellAlarm_4))[0]]);
-                                                               recipe.DwellAlarm_5          = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellAlarm_5))[0]]);
-                                                               recipe.DwellAlarm_6          = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellAlarm_6))[0]]);
-                                                               recipe.DwellAlarm_7          = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellAlarm_7))[0]]);
-                                                               recipe.DwellAlarm_8          = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellAlarm_8))[0]]);
-                                                               recipe.CoolingTime           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.CoolingTime))[0]]);
-                                                               recipe.CoolingTemperature    = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.CoolingTemperature))[0]]);
-                                                               recipe.RampTime_1            = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampTime_1))[0]]);
-                                                               recipe.RampTime_2            = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampTime_2))[0]]);
-                                                               recipe.RampTime_3            = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampTime_3))[0]]);
-                                                               recipe.RampTime_4            = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampTime_4))[0]]);
-                                                               recipe.RampTime_5            = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampTime_5))[0]]);
-                                                               recipe.RampTime_6            = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampTime_6))[0]]);
-                                                               recipe.RampTime_7            = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampTime_7))[0]]);
-                                                               recipe.RampTime_8            = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampTime_8))[0]]);
-                                                               recipe.RampAlarm_1           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampAlarm_1))[0]]);
-                                                               recipe.RampAlarm_2           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampAlarm_2))[0]]);
-                                                               recipe.RampAlarm_3           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampAlarm_3))[0]]);
-                                                               recipe.RampAlarm_4           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampAlarm_4))[0]]);
-                                                               recipe.RampAlarm_5           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampAlarm_5))[0]]);
-                                                               recipe.RampAlarm_6           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampAlarm_6))[0]]);
-                                                               recipe.RampAlarm_7           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampAlarm_7))[0]]);
-                                                               recipe.RampAlarm_8           = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.RampAlarm_8))[0]]);
-                                                               recipe.TemperatureSetpoint_1 = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.TemperatureSetpoint_1))[0]]);
-                                                               recipe.TemperatureSetpoint_2 = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.TemperatureSetpoint_2))[0]]);
-                                                               recipe.TemperatureSetpoint_3 = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.TemperatureSetpoint_3))[0]]);
-                                                               recipe.TemperatureSetpoint_4 = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.TemperatureSetpoint_4))[0]]);
-                                                               recipe.TemperatureSetpoint_5 = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.TemperatureSetpoint_5))[0]]);
-                                                               recipe.TemperatureSetpoint_6 = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.TemperatureSetpoint_6))[0]]);
-                                                               recipe.TemperatureSetpoint_7 = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.TemperatureSetpoint_7))[0]]);
-                                                               recipe.TemperatureSetpoint_8 = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.TemperatureSetpoint_8))[0]]);
-                                                               recipe.DwellTimeOffset_1     = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTimeOffset_1))[0]]);
-                                                               recipe.DwellTimeOffset_2     = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTimeOffset_2))[0]]);
-                                                               recipe.DwellTimeOffset_3     = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTimeOffset_3))[0]]);
-                                                               recipe.DwellTimeOffset_4     = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTimeOffset_4))[0]]);
-                                                               recipe.DwellTimeOffset_5     = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTimeOffset_5))[0]]);
-                                                               recipe.DwellTimeOffset_6     = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTimeOffset_6))[0]]);
-                                                               recipe.DwellTimeOffset_7     = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTimeOffset_7))[0]]);
-                                                               recipe.DwellTimeOffset_8     = double.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.DwellTimeOffset_8))[0]]);
-                                                               recipe.SegmentCounts         = short.Parse(col[_RecipeTitles.FindIndexes(nameof(PLC_Recipe.SegmentCounts))[0]]);
+                                                               var dic = _RecipeTitles.ToDictionary(x => x.Key, x => col[x.Value]);
+                                                               return recipe.SetByDictionary(dic);
                                                            }
                                                            catch (Exception ex)
                                                            {
                                                                Log.Error(ex, "ImportRecipe失敗");
                                                                return false;
                                                            }
-
-                                                           return true;
                                                        });
 
             return results;
@@ -351,8 +288,9 @@ public class DataoutputCSV
                                      type.GetProperty(nameof(ProcessInfo.IsFinished))?.GetName(Language) ?? nameof(ProcessInfo.IsFinished)
                                  }.Concat(recipe.Keys));
 
-        RecordTitles  = $"{string.Join(",", record.Keys)}";
-        _RecipeTitles = recipe.Keys.ToArray();
+        RecordTitles = $"{string.Join(",", record.Keys)}";
+        var i = -1;
+        _RecipeTitles = recipe.Keys.ToDictionary(x => x, _ => i++);
         RecipeTitles  = $"{string.Join(",", recipe.Keys)}";
         AlarmTitles   = $"{string.Join(",", logevent.Keys)}";
     }
