f33023fd89a0a26000c38e7e231f51dbdd28c6f7
wunmao <wunmao@live.com>
2022-10-20 21:49:29 +0800

    20221020

---
+++ GPGO_MultiPLCs/Models/KeyHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Input;
+
+namespace GPGO_MultiPLCs.Models
+{
+    public static class KeyHelper
+    {
+        public enum MapType : uint
+        {
+            MAPVK_VK_TO_VSC = 0x0,
+            MAPVK_VSC_TO_VK = 0x1,
+            MAPVK_VK_TO_CHAR = 0x2,
+            MAPVK_VSC_TO_VK_EX = 0x3,
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int ToUnicode(
+            uint wVirtKey,
+            uint wScanCode,
+            byte[] lpKeyState,
+            [Out, MarshalAs(UnmanagedType.LPWStr, SizeParamIndex = 4)]
+            StringBuilder pwszBuff,
+            int cchBuff,
+            uint wFlags);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetKeyboardState(byte[] lpKeyState);
+
+        [DllImport("user32.dll")]
+        public static extern uint MapVirtualKey(uint uCode, MapType uMapType);
+
+        public static bool GetCharFromKey(Key key, out char ch)
+        {
+            ch = ' ';
+
+            var virtualKey = KeyInterop.VirtualKeyFromKey(key);
+            var keyboardState = new byte[256];
+            GetKeyboardState(keyboardState);
+
+            var scanCode = MapVirtualKey((uint)virtualKey, MapType.MAPVK_VK_TO_VSC);
+            var stringBuilder = new StringBuilder(2);
+
+            var result = ToUnicode((uint)virtualKey, scanCode, keyboardState, stringBuilder, stringBuilder.Capacity, 0);
+            if (result is -1 or 0)
+            {
+                return false;
+            }
+
+            ch = stringBuilder[0];
+
+            return true;
+        }
+    }
+}
